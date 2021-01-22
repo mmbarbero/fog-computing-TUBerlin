432a3a7d080cc586746074feea1420759b9dfd2c
miguel <mbar@master.home>
2021-01-22 19:05:22 +0100

    Changes to Edge Device

--- EdgeDevice.py
+++ EdgeDevice.py
@@ -4,7 +4,7 @@ import zmq
 from threading import Thread
 from paho.mqtt import client as mqtt
 
-ENV_AWS_INSTANCE = "tcp://localhost:5555"
+AWS_INSTANCE = "tcp://localhost:5555"
 
 
 #MQTT Stuff
@@ -17,7 +17,7 @@ port = 1883
 #ZeroMQ Stuff
 context = zmq.Context()
 socket = context.socket(zmq.REQ)
-socket.connect(ENV_AWS_INSTANCE)
+socket.connect(AWS_INSTANCE)
 
 def connect():
     def onConnect(client,userdata, flags, rc):
@@ -34,25 +34,28 @@ def connect():
 latestSensorData1 = []
 latestSensorData2 = []
 def calcTrafficIntesity():
+    if (len(latestSensorData1) != 0):
+        avg1 = sum(latestSensorData1) / len(latestSensorData1)
+        print("The list contains " + str(latestSensorData1)[1:-1] +" and the average is "+ str(avg1))
+    else:
+        avg1 = 0
+    if (len(latestSensorData2) != 0):
+        avg2 = sum(latestSensorData2) / len(latestSensorData2)
+        print("The list contains " + str(latestSensorData2)[1:-1] +" and the average is "+ str(avg2))
+    else:
+        avg2 = 0
+    cloudMsg = str(avg1) + "," + str(avg2)
+    latestSensorData1.clear()
+    latestSensorData2.clear()
+
+    return cloudMsg  
+
+def sendDataToCloud():
     while True:
         time.sleep(10)
-        if (len(latestSensorData1) != 0):
-            avg1 = sum(latestSensorData1) / len(latestSensorData1)
-            print("The list contains " + str(latestSensorData1)[1:-1] +" and the average is "+ str(avg1))
-        else:
-            avg1 = 0
-        if (len(latestSensorData2) != 0):
-            avg2 = sum(latestSensorData2) / len(latestSensorData2)
-            print("The list contains " + str(latestSensorData2)[1:-1] +" and the average is "+ str(avg2))
-        else:
-            avg2 = 0
-        cloudMsg = str(avg1) + "," + str(avg2)
-        socket.send(cloudMsg.encode())
+        socket.send(calcTrafficIntesity().encode())
         cloudResponse = socket.recv()
         print(cloudResponse.decode())
-        latestSensorData1.clear()
-        latestSensorData2.clear()
-
 
 def subscribe(client: mqtt):
     def onMessage(client, userdata, msg):
@@ -68,7 +71,7 @@ def subscribe(client: mqtt):
 
 
 def run():
-    thread = Thread(target = calcTrafficIntesity)
+    thread = Thread(target = sendDataToCloud)
     thread.start()
     client = connect()
     subscribe(client)
