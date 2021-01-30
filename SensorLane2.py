1732a06145c89b0d83377ae645d94b28a684d3ea
miguel <migmarbar@gmail.com>
2021-01-30 18:15:12 +0100

    Added comments and some tweaks

--- SensorLane2.py
+++ SensorLane2.py
@@ -9,6 +9,7 @@ broker = 'broker.hivemq.com'
 topic = "/traffic/lane2"
 port = 1883
 
+## This generates random data depending on the args passed
 def generateData(args):
     args = args.upper()
     if (args == "HIGH"):
@@ -18,6 +19,7 @@ def generateData(args):
     elif(args == "LOW"):
        return random.randint(0,5)
 
+## Makes the connection to the MQTT broker
 def connect():
     def onConnect(client,userdata, flags, rc):
         if rc == 0:
@@ -30,10 +32,11 @@ def connect():
     client.connect(broker, port)
     return client
 
+## Every 10 seconds the data is generated and published to the broker
 def publish(client):
     if (len(sys.argv) > 1):
         while True:
-            time.sleep(2)
+            time.sleep(10)
             data = generateData(sys.argv[1])
             response = client.publish(topic, data)
             status = response[0]
