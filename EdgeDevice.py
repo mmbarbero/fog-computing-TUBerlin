6f6e7742de059c3cc00e26ed01e3788c712ef123
hittesharma <47592675+hittesharma@users.noreply.github.com>
2021-09-28 13:16:33 +0200

    Update EdgeDevice.py
    Adding code snippet in comment for data persistence.

--- EdgeDevice.py
+++ EdgeDevice.py
@@ -73,6 +73,24 @@ def subscribe(client: mqtt):
     client.subscribe(topic1)
     client.subscribe(topic2)
     client.on_message = onMessage
+    
+# To make data persistent in network partitioned systems we can utilize something like this: https://pypi.org/project/persizmq
+#
+#context = zmq.Context()
+#persistent_dir = pathlib.Path("/some/dir")
+#storage = persizmq.PersistentStorage(persistent_dir=persistent_dir)
+
+#def on_exception(exception: Exception)->None:
+#    print("an exception in the listening thread: {}".format(exception))
+
+#with persizmq.ThreadedSubscriber(
+#    callback=storage.add_message, subscriber=socket, 
+#    on_exception=on_exception):
+
+#    msg = storage.front()  # non-blocking
+#    if msg is not None:
+#        print("Received a persistent message: {}".format(msg))
+#        storage.pop_front()
 
 ## We run the average calculation and ZeroMQ data transfer on a thread in order to keep recienving the sensor data if the connection to cloud is down.
 def run():
